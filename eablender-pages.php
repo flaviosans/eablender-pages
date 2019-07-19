d894dc417a764c1dce02ce737d15107ba5a08a9d
Flavio Santos <flawinhoh@gmail.com>
2019-07-19 15:20:04 -0300

    :sparkles: Busca informações do usuário na API, via EABlender Budget

--- eablender-pages.php
+++ eablender-pages.php
@@ -8,54 +8,30 @@
  */
 
 class EABlender_Pages {
-	function __construct(){
-        $this->init();
 
-//		add_filter( 'page_template',    array($this, 'eablender_pages_load_custom_html' ) );
-		add_action('template_redirect', array($this, 'eablender_pages_show_page'));
-		add_filter('the_content',       array($this, 'eablender_pages_concatenate'));
+    private $api;
+
+	function __construct(){
+        $this->eablender_pages_init();
+
 		add_action('init',              array($this, 'eablender_pages_rewrite_rules' ));
+		add_filter('query_vars',        array($this, 'eablender_pages_add_querystring_vars' ));
+		add_filter('the_content',       array($this, 'eablender_generate_pages' ));
 	}
 
-	private function init(){
+	private function eablender_pages_init(){
 		if ( class_exists( 'EABlender_API' ) ) {
 			$this->api = EABlender_API::get_instance();
 		} else {
 			add_action( 'admin_notices', array($this, function(){
-				?>
-                    <div class="notice notice-warning">
-                        <p><?php _e( 'EABlender Pages: EABlender API parece não estar presente' ); ?></p>
-                    </div>
-				<?php
+            ?>
+                <div class="notice notice-warning">
+                    <p><?php _e( 'EABlender Pages: EABlender API parece não estar presente' ); ?></p>
+                </div>
+            <?php
 			}));
 		}
     }
-
-    public function eablender_pages_show_page(){
-	    if(strpos($_SERVER['REQUEST_URI'], 'encontre')){
-	        $path = explode("-", $_SERVER['REQUEST_URI']);
-            $professional = null;
-            if(!is_404()) {
-                return;
-            }
-        }
-
-	    return;
-    }
-
-	/**
-     * Carrega uma página customizada, baseada num Slug
-     *
-	 * @param $page_template
-	 *
-	 * @return string
-	 */
-	public function eablender_pages_load_custom_html( $page_template ){
-		if ( is_page( 'my-custom-page-slug' ) ) {
-			$page_template = plugin_dir_path(__FILE__) . 'page-professional.php';
-		}
-		return $page_template;
-	}
 
 	/**
 	 * Filtra uma postagem, substituindo as variáveis por atributos da cidade
@@ -64,17 +40,69 @@ class EABlender_Pages {
 	 *
 	 * @return mixed $content
 	 */
-	public function eablender_pages_concatenate($content){
-		$uri = $_SERVER['REQUEST_URI'];
-		$city = "Maringá";
+	public function eablender_generate_pages($content){
 
-	    return str_replace('%CIDADE%', $city, $content);
+		if(get_query_var('eablender_pages_user_app')){
+
+			$response = $this->api->get_user_app(get_query_var('eablender_pages_user_app'));
+
+			if($response->status_code = 200){
+
+			    $content = $this->eablender_pages_replace_vars($response->content, $content);
+            }
+
+			return $content;
+        }
     }
 
+	/**
+	 * Adiciona as urls personalizadas das páginas do APP
+	 */
 	function eablender_pages_rewrite_rules() {
-		$page_id = 9;
-		add_rewrite_rule('^encontre-[oa]s-melhores-arquitet[oa]s-em.*-[a-z]{2}', 'index.php?page_id=' . $page_id, 'top');
+
+		add_rewrite_rule(
+		        '^site\/(.*)',
+                'index.php?page_id=19&eablender_pages_user_app=$matches[1]',
+                'top'
+        );
+
+		flush_rewrite_rules();
 	}
+
+	/**
+     * Adiciona as querystrings necessárias para a página
+     *
+	 * @param $vars
+	 *
+	 * @return array
+	 */
+	public function eablender_pages_add_querystring_vars($vars){
+		$vars[] = 'eablender_pages_user_app';
+		return $vars;
+    }
+
+    private function eablender_pages_replace_vars($obj, $post_content){
+
+	    $var_name  = array(
+            '%ID%',
+            '%NAME%',
+            '%USERNAME%',
+            '%EMAIL%',
+            '%PHONE%',
+            '%DESCRIPTION%'
+        );
+
+        $var_value = array(
+            $obj->id,
+            $obj->name,
+            $obj->username,
+            $obj->email,
+            $obj->phone,
+            $obj->userAppDetails->description
+        );
+
+        return str_replace($var_name, $var_value, $post_content);
+    }
 }
 
 $eablender_pages = new EABlender_Pages;
